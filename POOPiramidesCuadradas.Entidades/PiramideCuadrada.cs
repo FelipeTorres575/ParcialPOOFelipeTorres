a601fa46706b0ba0b04bcdf34b2cdad99a23a532
Felipe Torres <felipetorres5752@gmail.com>
2023-07-10 20:10:35 -0300

    Agregar archivos de proyecto.

---
+++ POOPiramidesCuadradas.Entidades/PiramideCuadrada.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOPiramidesCuadradas.Entidades
+{
+    public class PiramideCuadrada
+    {
+        private double Lado;
+        private double Altura;
+        private double Arista;
+
+        public double Area;
+        public double Volumen;
+
+        public PiramideCuadrada(double lado, double altura)
+        {
+            Lado = lado;
+            Altura = altura;
+            Area = GetArea();
+            Volumen = GetVolumen();
+        }
+
+        public double GetArista()
+        {
+            return Math.Sqrt(Math.Pow(Altura, 2) + Math.Pow(Lado, 2) / 2);
+        }
+
+        public double GetArea()
+        {
+            return Lado * (Lado + Math.Sqrt(4 * Math.Pow(Altura, 2) + Math.Pow(Lado, 2)));
+        }
+
+        public double GetVolumen()
+        {
+            return (Math.Pow(Lado, 2) * Altura) / 3;
+        }
+
+        public override string ToString()
+        {
+            return $"Lado={Lado},Arista={Arista}, Altura={Altura} ,Area={Area}, Volumen={Volumen}";
+        }
+
+    }
+}
---
+++ POOPiramidesCuadradas/Program.cs
@@ -0,0 +1,19 @@
+﻿using POOPiramidesCuadradas.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOPiramidesCuadradas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PiramideCuadrada pc = new PiramideCuadrada(2,4);
+            
+            Console.WriteLine(pc.ToString());
+        }
+    }
+}
